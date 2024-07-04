b433a58407229dfadbb48433cfe93207d319c959
Jhosue-Chica <jichica@espe.edu.ec>
2024-07-04 11:04:16 -0500

    binding

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './binding.component.html',
-  styleUrl: './binding.component.css'
+  styleUrls: ['./binding.component.css']
 })
 export class BindingComponent {
   title = 'Jhosue Chica';
@@ -15,6 +16,19 @@ export class BindingComponent {
   imagenUrl = '../../user.png';
 
   twoWayDataBindig = '';
+
+  // Ejemplo 2: Selector (Dropdown)
+  selectedFruit: string = '';
+  fruits: string[] = ['Manzana', 'Banana', 'Cereza', 'Datil'];
+
+  // Ejemplo 3: Checkbox
+  isSubscribed: boolean = false;
+
+  // Ejemplo 4: Textarea
+  message: string = '';
+
+  // Ejemplo 5: Radio Buttons
+  gender: string = '';
 
   onClick(){
     console.log('Click en el boton');
